6d890237fd5baea48885e29f9bdda95b236007ff
CustomLZH <custom_coming@163.com>
2022-12-31 14:28:46 +0800

    基础环境

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,21 @@
 
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
+  parserOptions: { // 对新语法使用eslint
+    parser: 'babel-eslint',
+    ecmaVersion: 2023,
+    sourceType: 'module'
   },
   env: {
     browser: true,
+    es6: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential', 
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'standard',
   ],
   // required to lint *.vue files
   plugins: [
@@ -24,6 +27,12 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'always'],
+    // 末尾禁止添加逗号
+    'comma-dangle': ['error', 'always-multiline'],
+    // 强制使用一致的缩进
+    'indent': ['error', 2],
+    'semi-style': ['error', 'last'],
   }
 }
